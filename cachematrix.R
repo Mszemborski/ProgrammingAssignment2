60bac601f03d30377c6e507a09558d5b61000adf
Mszemborski <86018342+Mszemborski@users.noreply.github.com>
2021-06-27 14:55:34 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## SetInverse x as matrix
+## and solved the value "j" as null
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix)(sample(1:100,9,3,3)) {
+j <- NULL
+set <- function(y){
+  x<<-y
+  j<<- NULL
+}
+get <-function()x
+  setInverse <-function(inverse)j<<-inverse
+getInverse<-function(j)
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Changed "m" to "j"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  j<-x$getInverse()
+  if(!is.null(j)){
+    message("getting cache data")
+    return(j)
+  }
+  data<-x$get()
+  j<-solve(data, ...)
+  x$setInverse(j)
+  j
+}
+
+inv<-NULL
+set<-function(y){
+  x<<-y
+  inv<<-NULL
+}
+get<-function()x
+setinv<-function(inverse) inv<<-inverse
+getinv<-function()inv
+list(set=set,get=get,setinv=setinv,getinv=getinv)
+
+j<-matrixrnorm(16(44))
+cachesolve(j1)
+[,1][,2][,3][,4]
+
 }
